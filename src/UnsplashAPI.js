72f7e866680160fe9eead4fcae359fc95f4b4b00
Sergvah <104735864+Sergvah@users.noreply.github.com>
2022-10-16 15:34:19 +0300

    task-11 async/await

--- src/UnsplashAPI.js
+++ src/UnsplashAPI.js
@@ -1,24 +1,20 @@
+import axios from 'axios';
+axios.defaults.baseURL = 'https://pixabay.com/api/';
+// const BASE_URL = 'https://pixabay.com/api/';
+const KEY = '30566822-5dd8c7f8088312f63e039c329';
+const params = `?key=${KEY}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40`;
+
 export class UnsplashAPI {
   //   API_KEY = '30566822-5dd8c7f8088312f63e039c329';
   #page = 1;
   #searchQuery = '';
   #totalPages = 0;
   #perPage = 40;
-  getPhotos() {
-    URL = `https://pixabay.com/api/?key=30566822-5dd8c7f8088312f63e039c329`;
-
-    return fetch(
-      `${URL}&q=${
-        this.#searchQuery
-      }&image_type=photo&orientation=horizontal&safesearch=true&per_page=${
-        this.#perPage
-      }&page=${this.#page}`
-    ).then(response => {
-      if (!response.ok) {
-        throw new Error(response.status);
-      }
-      return response.json();
-    });
+  async getPhotos() {
+    const response = await axios.get(
+      `${params}&q=${this.#searchQuery}&page=${this.#page}`
+    );
+    return response.data;
   }
   set searchQuery(newQuery) {
     this.#searchQuery = newQuery;
--- src/index.js
+++ src/index.js
@@ -8,9 +8,7 @@ const photoGallery = document.querySelector('.gallery');
 const searchForm = document.querySelector('#search-form');
 const loadmoreBtn = document.querySelector('.load-more');
 
-searchForm.addEventListener('submit', onSearchForm);
-
-function onSearchForm(event) {
+const onSearchForm = async event => {
   //Чтобы не перезагружалась страница
   event.preventDefault();
   const {
@@ -25,94 +23,82 @@ function onSearchForm(event) {
   }
   unsplash.searchQuery = searchQueryData;
   clearPage();
-  unsplash
-    .getPhotos()
-    .then(({ hits, total }) => {
-      if (hits.length === 0) {
-        Notify.failure(
-          'Sorry, there are no images matching your search query. Please try again.'
-        );
-        return;
-      }
-      const dataOfPhotos = hits
-        .map(photo => {
-          return `<div class="photo-card">
-  <img src="${photo.webformatURL}" alt="${photo.tags}" class="img-gallery" loading="lazy" />
-  <div class="info">
-    <p class="info-item">
-      <b>Likes ${photo.likes}</b>
-    </p>
-    <p class="info-item">
-      <b>Views ${photo.views}</b>
-    </p>
-    <p class="info-item">
-      <b>Comments ${photo.comments}</b>
-    </p>
-    <p class="info-item">
-      <b>Downloads ${photo.downloads}</b>
-    </p>
-  </div>
-</div>`;
-        })
-        .join('');
-      photoGallery.insertAdjacentHTML('beforeend', dataOfPhotos);
-      unsplash.calculateTotalPages(total);
-      Notify.success(`Hooray! We found ${total} images.`);
-      console.log(unsplash);
-      if (unsplash.isShowLoadMore) {
-        loadmoreBtn.classList.remove('is-hidden');
-      }
-    })
-    .catch(error => {
-      Notify.failure(error.message, 'ERROR');
-      clearPage();
-    });
-}
-const onLoadMore = () => {
-  unsplash.incrementPage();
-  if (!unsplash.isShowLoadMore) {
-    loadmoreBtn.classList.add('is-hidden');
-    Notify.info(`We're sorry, but you've reached the end of search results.`);
-  }
-  unsplash
-    .getPhotos()
-    .then(({ hits }) => {
-      //   if (hits.length === 0) {
-      //     Notify.info(
-      //       `We're sorry, but you've reached the end of search results.`
-      //     );
-      //     return;
-      //   }
-      const dataOfPhotos = hits
-        .map(photo => {
-          return `<div class="photo-card">
-  <img src="${photo.webformatURL}" alt="${photo.tags}" class="img-gallery" loading="lazy" />
-  <div class="info">
-    <p class="info-item">
-      <b>Likes ${photo.likes}</b>
-    </p>
-    <p class="info-item">
-      <b>Views ${photo.views}</b>
-    </p>
-    <p class="info-item">
-      <b>Comments ${photo.comments}</b>
-    </p>
-    <p class="info-item">
-      <b>Downloads ${photo.downloads}</b>
-    </p>
-  </div>
-</div>`;
-        })
-        .join('');
-      photoGallery.insertAdjacentHTML('beforeend', dataOfPhotos);
-      // console.log(dataOfPhotos);
 
-      // console.log(bankOfPhotos());
-    })
-    .catch(error => {
-      Notify.failure(error.message, 'ERROR');
-      clearPage();
-    });
+  try {
+    const { hits, total } = await unsplash.getPhotos();
+    if (hits.length === 0) {
+      Notify.failure(
+        'Sorry, there are no images matching your search query. Please try again.'
+      );
+      return;
+    }
+    const dataOfPhotos = hits
+      .map(photo => {
+        return `<div class="photo-card">
+  <img src="${photo.webformatURL}" alt="${photo.tags}" class="img-gallery" loading="lazy" />
+  <div class="info">
+    <p class="info-item">
+      <b class="b-item">Likes</b> ${photo.likes}
+    </p>
+    <p class="info-item">
+      <b class="b-item">Views</b> ${photo.views}
+    </p>
+    <p class="info-item">
+      <b class="b-item">Comments</b> ${photo.comments}
+    </p>
+    <p class="info-item">
+      <b class="b-item">Downloads</b> ${photo.downloads}
+    </p>
+  </div>
+</div>`;
+      })
+      .join('');
+    photoGallery.insertAdjacentHTML('beforeend', dataOfPhotos);
+    unsplash.calculateTotalPages(total);
+    Notify.success(`Hooray! We found ${total} images.`);
+    console.log(unsplash);
+    if (unsplash.isShowLoadMore) {
+      loadmoreBtn.classList.remove('is-hidden');
+    }
+  } catch (error) {
+    Notify.failure(error.message, 'ERROR');
+    clearPage();
+  }
+};
+const onLoadMore = async () => {
+  unsplash.incrementPage();
+  try {
+    const { hits } = await unsplash.getPhotos();
+    if (!unsplash.isShowLoadMore) {
+      loadmoreBtn.classList.add('is-hidden');
+      Notify.info(`We're sorry, but you've reached the end of search results.`);
+    }
+    const dataOfPhotos = hits
+      .map(photo => {
+        return `<div class="photo-card">
+  <img src="${photo.webformatURL}" alt="${photo.tags}" class="img-gallery" loading="lazy" />
+  <div class="info">
+    <p class="info-item">
+      <b class="b-item">Likes ${photo.likes}</b>
+    </p>
+    <p class="info-item">
+      <b class="b-item">Views ${photo.views}</b>
+    </p>
+    <p class="info-item">
+      <b class="b-item">Comments ${photo.comments}</b>
+    </p>
+    <p class="info-item">
+      <b class="b-item">Downloads ${photo.downloads}</b>
+    </p>
+  </div>
+</div>`;
+      })
+      .join('');
+    photoGallery.insertAdjacentHTML('beforeend', dataOfPhotos);
+  } catch (error) {
+    Notify.failure(error.message, 'ERROR');
+    clearPage();
+  }
 };
 
 function clearPage() {
@@ -121,6 +107,7 @@ function clearPage() {
   loadmoreBtn.classList.add('is-hidden');
 }
 loadmoreBtn.addEventListener('click', onLoadMore);
+searchForm.addEventListener('submit', onSearchForm);
 
 // function filteredList(allPhotos) {
 //   const filter = searchForm.value;
